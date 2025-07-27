103ae61f3722c87f4b5099ecfa7658d8791013f4
SangamChaurasiya <sangam.chaurasiya0604@gmail.com>
2025-07-27 13:33:19 +0530

    Path parameter validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is the entry point for our application.
 from enum import Enum
-from fastapi import FastAPI, status, Query
+from fastapi import FastAPI, status, Query, Path
 from typing import Annotated
 from pydantic import AfterValidator
 
@@ -172,14 +172,49 @@ PRODUCTS = [
 #     return PRODUCTS
 
 
-# Custom Validation
-def check_valid_id(id: str):
-    if not id.startswith("prod-"):
-        raise ValueError("ID must start with 'prod-'")
-    return id
+# # Custom Validation
+# def check_valid_id(id: str):
+#     if not id.startswith("prod-"):
+#         raise ValueError("ID must start with 'prod-'")
+#     return id
 
-@app.get("/products")
-async def get_products(id: Annotated[str | None, AfterValidator(check_valid_id)]=None):
-    if id:
-        return {"id": id, "message": "Valid product ID"}
-    return {"message": "No ID provided"}
+# @app.get("/products")
+# async def get_products(id: Annotated[str | None, AfterValidator(check_valid_id)]=None):
+#     if id:
+#         return {"id": id, "message": "Valid product ID"}
+#     return {"message": "No ID provided"}
+
+
+# # Basic Path Parameter
+# @app.get("/products/{product_id}")
+# async def get_product(product_id: int):
+#     for product in PRODUCTS:
+#         if product["id"] == product_id:
+#             return product
+#     return {"error": "Product not found"}
+
+# # Numeric Validation for Path Parameter
+# @app.get("/products/{product_id}")
+# async def get_product(product_id: Annotated[int, Path(ge=1)]):
+#     for product in PRODUCTS:
+#         if product["id"] == product_id:
+#             return product
+#     return {"error": "Product not found"}
+
+# # Adding Metadata with Path
+# @app.get("/products/{product_id}")
+# async def get_product(product_id: Annotated[int, Path(title="The ID of the product", description="This is Product ID")]):
+#     for product in PRODUCTS:
+#         if product["id"] == product_id:
+#             return product
+#     return {"error": "Product not found"}
+
+# Combining Path and Query Parameter
+@app.get("/products/{product_id}")
+async def get_product(product_id: Annotated[int, Path(gt=0, le=100)], search: Annotated[str|None, Query(max_length=20)] = None):
+    for product in PRODUCTS:
+        if product["id"] == product_id:
+            if search and search.lower() not in product["title"].lower():
+                return {"error": "Product does not match search term"}
+            return product
+    return {"error": "Product not found"}
